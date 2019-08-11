8cbc2ee97f069c1b5ae0987697fc0ff29698fe83
zengyunlong <273412935@qq.com>
2019-08-11 20:22:20 +0800

    service call service

--- app/service/news.js
+++ app/service/news.js
@@ -7,6 +7,11 @@ class NewsService extends Service {
     // 注意获取新闻数据
 
     let list = ['1111', '2222', '3333'];
+
+    // 调用 service 中的其它（user) 服务的数据
+    let user = await this.service.user.getUserInfo();
+    console.log(user);
+
     return list;
   }
 }
